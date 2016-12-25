de924ce8d83adc32d73510a5c59894143c3f5e54
Masaya Kazama <miyan@t-p.jp>
2016-12-26 07:18:17 +0900

    Add emoji

--- keyboards/ergodox/keymaps/miyaoka/keymap.c
+++ keyboards/ergodox/keymaps/miyaoka/keymap.c
@@ -4,6 +4,8 @@
 #include "ergodox.h"
 #include "debug.h"
 #include "action_layer.h"
+#include "process_unicode.h"
+#include "quantum.h"
 
 //Alias
 #define C(kc) LCTL(kc)
@@ -64,6 +66,110 @@ enum macros {
   T_KANA
 };
 
+// unicode map
+
+enum unicode_name {
+  SUSHI, //🍣
+  GRIN, // grinning face 😊
+  TJOY, // tears of joy 😂
+  SMILE, // grining face with smiling eyes 😁
+  HEART, // heart ❤
+  EYERT, // smiling face with heart shaped eyes 😍
+  CRY, // crying face 😭
+  SMEYE, // smiling face with smiling eyes 😊
+  UNAMU, // unamused 😒
+  KISS, // kiss 😘
+  HART2, // two hearts 💕
+  WEARY, // weary 😩
+  OKHND, // ok hand sign 👌
+  PENSV, // pensive 😔
+  SMIRK, // smirk 😏
+  RECYC, // recycle ♻
+  WINK, // wink 😉
+  THMUP, // thumb up 👍
+  THMDN, // thumb down 👎
+  PRAY, // pray 🙏
+  PHEW, // relieved 😌
+  MUSIC, // musical notes
+  FLUSH, // flushed 😳
+  CELEB, // celebration 🙌
+  CRY2, // crying face 😢
+  COOL, // smile with sunglasses 😎
+  NOEVS, // see no evil
+  NOEVH, // hear no evil
+  NOEVK, // speak no evil
+  POO, // pile of poo
+  EYES, // eyes
+  VIC, // victory hand
+  BHART, // broken heart
+  SLEEP, // sleeping face
+  SMIL2, // smiling face with open mouth & sweat
+  HUNRD, // 100
+  CONFU, // confused
+  TONGU, // face with tongue & winking eye
+  DISAP, // disappointed
+  YUMMY, // face savoring delicious food
+  CLAP, // hand clapping
+  FEAR, // face screaming in fear
+  HORNS, // smiling face with horns
+  HALO, // smiling face with halo
+  BYE, // waving hand
+  SUN, // sun
+  MOON, // moon
+  SKULL, // skull
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+  [SUSHI] = 0x1F363,
+  [GRIN] = 0x1F600,
+  [TJOY] = 0x1F602,
+  [SMILE] = 0x1F601,
+  [HEART] = 0x2764,
+  [EYERT] = 0x1f60d,
+  [CRY] = 0x1f62d,
+  [SMEYE] = 0x1F60A,
+  [UNAMU] = 0x1F612,
+  [KISS] = 0x1F618,
+  [HART2] = 0x1F495,
+  [WEARY] = 0x1F629,
+  [OKHND] = 0x1F44C,
+  [PENSV] = 0x1F614,
+  [SMIRK] = 0x1F60F,
+  [RECYC] = 0x267B,
+  [WINK] = 0x1F609,
+  [THMUP] = 0x1F44D,
+  [THMDN] = 0x1F44E,
+  [PRAY] = 0x1F64F,
+  [PHEW] = 0x1F60C,
+  [MUSIC] = 0x1F3B6,
+  [FLUSH] = 0x1F633,
+  [CELEB] = 0x1F64C,
+  [CRY2] = 0x1F622,
+  [COOL] = 0x1F60E,
+  [NOEVS] = 0x1F648,
+  [NOEVH] = 0x1F649,
+  [NOEVK] = 0x1F64A,
+  [POO] = 0x1F4A9,
+  [EYES] = 0x1F440,
+  [VIC] = 0x270C,
+  [BHART] = 0x1F494,
+  [SLEEP] = 0x1F634,
+  [SMIL2] = 0x1F605,
+  [HUNRD] = 0x1F4AF,
+  [CONFU] = 0x1F615,
+  [TONGU] = 0x1F61C,
+  [DISAP] = 0x1F61E,
+  [YUMMY] = 0x1F60B,
+  [CLAP] = 0x1F44F,
+  [FEAR] = 0x1F631,
+  [HORNS] = 0x1F608,
+  [HALO] = 0x1F607,
+  [BYE] = 0x1F44B,
+  [SUN] = 0x2600,
+  [MOON] = 0x1F314,
+  [SKULL] = 0x1F480,
+};
+
 uint16_t hold_timers[MATRIX_ROWS][MATRIX_COLS];
 
 // Fillers to make layering clearer
@@ -100,7 +206,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,     _______,    _______,    _______,    _______,    _______,    KC_LBRC,
   KC_LCTL,    _______,    _______,    _______,    _______,    _______,
   KC_LSFT,    _______,    _______,    _______,    _______,    _______,    KC_TAB,
-  _______,    _______,    KC_BSLS,    KC_GRV,     SFT_T(KC_MINS),
+  X(SUSHI),    _______,    KC_BSLS,    KC_GRV,     SFT_T(KC_MINS),
 
               _______,    M(T_EISU),
                           _______,
@@ -112,7 +218,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_RBRC,    _______,    _______,    _______,    _______,    _______,    KC_CAPS,
               _______,    _______,    _______,    _______,    _______,    KC_RCTL,
   CTL_T(KC_QUOT),    _______,    _______,    _______,    _______,    _______,    KC_RSFT,
-                          SFT_T(KC_EQL),    KC_BSPC,    KC_DEL,     _______,    _______,
+                          SFT_T(KC_EQL),    KC_BSPC,    KC_DEL,     _______,    X(SUN),
 
   _______,    _______,
   _______,
@@ -565,12 +671,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     //os
     case MAC:
+      set_unicode_input_mode(UC_OSX);
       if (record->event.pressed) {
         persistant_default_layer_set(1UL<<L_MAC);
       }
       return false;
       break;
     case WIN:
+      set_unicode_input_mode(UC_WINC);
       if (record->event.pressed) {
         persistant_default_layer_set(1UL<<L_WIN);
       }
@@ -613,6 +721,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
+  set_unicode_input_mode(UC_OSX);
   persistant_default_layer_set(1UL<<L_MAC);
   layer_move(L_QWDR);
 };
